5f9de8c011f2ee6fbc39787c6f7f89f53619c7f7
MarioKreticoskiJunyor <mariokreticoskij@gmail.com>
2024-02-14 13:16:33 -0300

    estudo dia 14

--- 2_funcoes/aula97.py
+++ 2_funcoes/aula97.py
@@ -1,42 +1,18 @@
-# Módulos padrão do Python (import, from, as e *)
-# https://docs.python.org/3/py-modindex.html
-# inteiro - import nome_modulo
-# Vantagens: você tem o namespace do módulo
-# Desvantagens: nomes grandes
-# import sys
+# Modularização - Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outro módulo inteiro ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastas
+# abaixo dele.
+# Ele não reconhece pastas e módulos acima do __main__ por
+# padrão
+# O python conhece todos os módulos e pacotes presentes
+# nos caminhos de sys.path
 
-# platform = 'A MINHA'
-# print(sys.platform)
-# print(platform)
+import aula97_m
+from aula97_m import variavel_modulo , soma
 
-# partes - from nome_modulo import objeto1, objeto2
-# Vantagens: nomes pequenos
-
-# Desvantagens: Sem o namespace do módulo
-# from sys import exit, platform
-
-# print(platform)
-
-# alias 1 - import nome_modulo as apelido
-# import sys as s
-# sys ='Alguma coisa'
-# print(s.platform)
-# print(sys)
-
-# alias 2 - from nome_modulo import objeto as apelido
-# from sys import exit as ex
-# from sys import platform as pf 
-
-
-# print(pf)
-
-# Vantagens: você pode reservar nomes para seu código
-# Desvantagens: pode ficar fora do padrão da linguagem
-
-# má prática - from nome_modulo import *
-# Vantagens: importa tudo de um módulo
-# Desvantagens: importa tudo de um módulo
-# from sys import exit, platform
-
-# print(platform)
-# exit()
+# print('Este modulo se chama', __name__)
+print(aula97_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2,3))
+print(aula97_m.soma(2,3))
---
+++ 2_funcoes/aula99.py
@@ -0,0 +1,22 @@
+# from sys import path
+
+
+# import aula99_package.modulo
+# from aula99_package import modulo
+# from aula99_package.modulo import soma_do_modulo
+
+
+
+# # print(*path, sep='\n')
+# print(soma_do_modulo(1,2))
+# print(aula99_package.modulo.soma_do_modulo(1,2))
+# from aula99_package.modulo import soma_do_modulo,fala_oi
+
+
+# print(__name__)
+# fala_oi()
+
+from aula99_package import soma_do_modulo,falar_oi
+
+print(soma_do_modulo(2,3))
+falar_oi()
