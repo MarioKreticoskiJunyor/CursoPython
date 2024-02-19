598dca65ab15f2541556adf4bbd0bd140877ef02
MarioKreticoskiJunyor <mariokreticoskij@gmail.com>
2024-02-19 18:34:52 -0300

    estudo 19-02-2024

--- 2_funcoes/aula109.py
+++ 2_funcoes/aula109.py
@@ -2,3 +2,25 @@
 # Combinação - Ordem não importa - iterável + tamanho do grupo
 # Permutação - Ordem importa
 # Produto - Ordem importa e repete valores únicos
+
+from itertools import combinations, permutations,product
+
+
+def print_iter(interator):
+    print(*list(interator),sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unisex'],
+    ['algodão', 'poliéster']
+]
+
+print_iter(combinations(pessoas,2))
+print_iter(permutations(pessoas,2))
+print_iter(product(*camisetas))
